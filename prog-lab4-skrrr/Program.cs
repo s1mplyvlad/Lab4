033c1dbf883128f2a3fbded03d5a1a3f14a3834e
Vladyslav <51118938+s1mplyvlad@users.noreply.github.com>
2019-05-28 14:28:32 +0300

    Add files via upload

---
+++ prog-lab4-skrrr/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace prog_lab4_skrrr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1, s2;
+            Console.WriteLine("Введите первую строку : ");
+            s1 = Console.ReadLine();
+            Console.WriteLine("Введите вторую строку : ");
+            s2 = Console.ReadLine();
+            Console.WriteLine(String.Compare("a", "b"));
+            int b = String.Compare(s1, s2);
+            if (b == 0)
+            {
+                Console.WriteLine("Строки совпадают");
+            }
+
+            else { Console.WriteLine("Строки не совпадают"); }
+        }
+
+    }
+}
